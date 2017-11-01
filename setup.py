4f7942e7f813b5b1623222183a7d22c5c2817de6
Johnny Mariéthoz <johnny.mariethoz@rero.ch>
2017-11-01 21:38:18 +0100

    installation: fix dependencies
    Signed-off-by: Johnny Mariéthoz <johnny.mariethoz@rero.ch>

--- setup.py
+++ setup.py
@@ -60,11 +60,9 @@ setup_requires = [
 ]
 
 install_requires = [
-    'celery<4.0,>=3.1',                 # FIXME: invenio-indexer
-    'elasticsearch<3.0.0,>=2.0.0',      # FIXME: invenio-search
-    'elasticsearch-dsl<3.0.0,>=2.0.0',  # FIXME: invenio-search
-    'Flask-BabelEx>=0.9.2',
     'Flask>=0.11.1',
+    'Flask-BabelEx>=0.9.2',
+    'Flask-CeleryExt>=0.3.0',
     'invenio-assets>=1.0.0b2',
     'invenio-db[versioning, postgresql]>=1.0.0b1',
     'invenio-indexer>=1.0.0a6',
